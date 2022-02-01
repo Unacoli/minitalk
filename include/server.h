7bb3edd031ff7bfd6cbf26a9dfdad28e3f947953
Nao ARGOUSE <nargouse@student.42.fr>
2022-02-01 20:27:28 +0100

    server handling reception of signals, printing bit per bit

--- include/server.h
+++ include/server.h
@@ -6,7 +6,7 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 17:22:49 by nargouse          #+#    #+#             */
-/*   Updated: 2022/02/01 16:01:26 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:04:16 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 # define SERVER_H
 # define BUFF_SIZE 1024
 
-#include "../libft/include/libft.h"
-#include "../libft/include/ft_printf.h"
-#include <unistd.h>
-#include <signal.h>
+# include "../libft/include/libft.h"
+# include "../libft/include/ft_printf.h"
+# include <unistd.h>
+# include <signal.h>
 
-extern int *g_buffer;
+extern int	g_buffer[8];
 
-void	ft_quit(char *message);
+void		ft_quit(char *message);
 
-int	*ft_dec_to_bin(int n);
-int	ft_bin_to_dec(int *n);
+int			ft_bin_to_dec(int *n);
 
 #endif
--- srcs/main_server.c
+++ srcs/main_server.c
@@ -6,66 +6,45 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 17:20:26 by nargouse          #+#    #+#             */
-/*   Updated: 2022/02/01 19:13:38 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:15:13 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-int	*g_buffer;
+int	g_buffer[8];
 
-static	char *malloc_tab(char *str)
+static void	ft_decode_signals()
 {
-	g_buffer = (int*)(malloc(sizeof(int) * 8));
-	if (g_buffer == NULL)
-	{
-		free(g_buffer);
-		ft_quit("Malloc allocation error\n");
-	}
-	str = (char *)malloc(sizeof(char) * BUFF_SIZE);
-	if (str == NULL)
-	{
-		free(str);
-		ft_quit("Malloc allocation error\n");
-	}
-	ft_bzero(g_buffer, 8);
-	return (str);
-}
-
-static char	*ft_decode_signals(char *str, int *tmp)
-{
-	tmp = NULL;
-	str = NULL;
-
-	ft_printf("%d\n", g_buffer);
-	return (str);
+	;
 }
 
 static void	signal_handler(int sig)
 {
-	*g_buffer++ = sig;
+	if (sig == SIGUSR1)
+	{
+		ft_printf("1");
+	}
+	else
+	{
+		ft_printf("0");
+	}
+	ft_printf("\n");
 }
 
 int	main(void)
 {
 	int		i;
-	int		*tmp;
-	char	*str;
 
     ft_printf("%d\n", getpid());
 	i = 0;
-	str = NULL;
-	str = malloc_tab(str);
-	tmp = g_buffer;
 	signal(SIGUSR1, &signal_handler);
 	signal(SIGUSR2, &signal_handler);
 	while (1)
 	{
-		ft_printf("noot bv bvvmbvcbnc\n");
 		if (i == 8)
 		{
-			ft_printf("noot\n");
-			str = ft_decode_signals(str, tmp);
+			ft_decode_signals();
 			i = 0;
 		}
 		pause();
